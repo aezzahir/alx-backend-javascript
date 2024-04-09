3bd060fb7dec96a4946a877d1cd725cc38582b12
aezzahir <aero.glowsky@gmail.com>
2024-04-09 17:04:24 +0000

    java script

---
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -0,0 +1,10 @@
+export default function createReportObject(employeesList) {
+  return {
+    allEmployees: {
+      ...employeesList,
+    },
+    getNumberOfDepartments(employees) {
+      return Object.keys(employees).length;
+    },
+  };
+}
