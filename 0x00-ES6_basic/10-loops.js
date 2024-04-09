b99e24e067965f2cff1cfbc411322079d0e62b1d
aezzahir <aero.glowsky@gmail.com>
2024-04-09 16:58:14 +0000

    java script

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,9 +1,7 @@
 export default function appendToEachArrayValue(array, appendString) {
-  const newArray = [...array];
-  for (const value of newArray) {
-    const idx = newArray.indexOf(value);
-    newArray[idx] = appendString + value;
+  const newArray = [];
+  for (const value of array) {
+    newArray.push(appendString + value);
   }
-
   return newArray;
 }
