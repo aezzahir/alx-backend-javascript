304c82d61d1bde03be2b95e730bac25f870af8cb
aezzahir <aero.glowsky@gmail.com>
2024-04-11 23:24:29 +0000

    3. Methods, static methods, computed methods names..... MONEY

---
+++ 0x02-ES6_classes/3-currency.js
@@ -0,0 +1,37 @@
+export default class Currency {
+  constructor(code, name) {
+    if (typeof name !== 'string' || typeof code !== 'string') {
+      throw new TypeError('Type Error');
+    }
+    this._name = name;
+    this._code = code;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  set name(newName) {
+    if (typeof newName === 'string') {
+      this._name = newName;
+    } else {
+      throw new TypeError('Type Error');
+    }
+  }
+
+  set code(newCode) {
+    if (typeof newCode === 'string') {
+      this._code = newCode;
+    } else {
+      throw new TypeError('Type Error');
+    }
+  }
+
+  displayFullCurrency() {
+    return `${this._name}(${this._code})`;
+  }
+}
