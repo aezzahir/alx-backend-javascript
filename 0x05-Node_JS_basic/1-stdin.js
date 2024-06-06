181189a2c76acc5ea277ff3cc716935d44ed0d5b
aezzahir <aero.glowsky@gmail.com>
2024-06-06 21:28:55 +0100

    console

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,13 +1,9 @@
 process.stdout.write("Welcome to Holberton School, what is your name?\n");
-process.stdin.setEncoding("utf8");
 
-process.stdin.on("readable", function () {
-  var name = process.stdin.read();
-  if (name !== null) {
-    process.stdout.write("Your name is: " + name);
-  }
+process.stdin.on("data", (data) => {
+  process.stdout.write(`Your name is: ${data}`);
 });
 
-process.on("exit", function () {
+process.stdin.on("end", () => {
   process.stdout.write("This important software is now closing\n");
 });
