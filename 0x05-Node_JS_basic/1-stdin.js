8f8a73bccd50a2605d909d2c30f9a93dbcb6724e
aezzahir <aero.glowsky@gmail.com>
2024-06-10 15:50:19 +0000

    Node

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,13 +1,10 @@
-process.stdin.setEncoding("utf8");
 process.stdout.write("Welcome to Holberton School, what is your name?\n");
 
-process.stdin.on("readable", function () {
-  var name = process.stdin.read();
-  if (name !== null) {
-    process.stdout.write(`Your name is: ${name}`);
-  }
+process.stdin.on("data", (data) => {
+  process.stdout.write(`Your name is: ${data}`);
 });
 
-process.stdin.on("end", function () {
+process.stdin.on("end", () => {
   process.stdout.write("This important software is now closing\n");
 });
+
