4e2988070cbc04933ca09f1aed2f199d601d0a8d
aezzahir <aero.glowsky@gmail.com>
2024-06-10 16:04:04 +0000

    3

---
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -0,0 +1,39 @@
+const fs = require("fs");
+
+function countStudents (path) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(path, "utf8", (error, data) => {
+      if (error) {
+        reject(new Error("Cannot load the database"));
+      } else {
+        const lines = data.trim().split("\n"),
+         students = lines.slice(1), // Exclude the header line
+         numberOfStudents = students.length;
+        console.log(`Number of students: ${numberOfStudents}`);
+
+        const fields = {};
+        for (const student of students) {
+          const [firstname, , , field] = student.split(",");
+          if (firstname !== "") {
+            if (fields[field]) {
+              fields[field].push(firstname);
+            } else {
+              fields[field] = [firstname];
+            }
+          }
+        }
+
+        for (const [field, students] of Object.entries(fields)) {
+          const numberOfStudentsInField = students.length,
+           studentList = students.join(", ");
+          console.log(`Number of students in ${field}: ${numberOfStudentsInField}. List: ${studentList}`);
+        }
+
+        resolve();
+      }
+    });
+  });
+}
+
+module.exports = countStudents;
+
