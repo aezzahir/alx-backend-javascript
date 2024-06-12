30b2df6b5196064fb8c1d70bc65822a2c59525bc
aezzahir <aero.glowsky@gmail.com>
2024-06-12 19:14:25 +0100

    6 - http express js

--- 0x05-Node_JS_basic/6-http_express.js
+++ 0x05-Node_JS_basic/6-http_express.js
@@ -1,13 +1,22 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 const port = 1245;
+
 app.get('/', (req, res) => {
   res.status(200).send('Hello Holberton School!');
 });
-app.use((req, res, next) => {
-  res.status(404).send("Sorry can't find that!");
+
+// Serve static files from the current directory
+app.use(express.static(__dirname));
+
+app.use((req, res) => {
+  res.status(404).sendFile(path.join(__dirname, 'not_found.html'));
 });
+
 app.listen(port, () => {
-  console.log(`App listening on port ${port}`);
+  console.log(`Server is listening on port ${port}`);
 });
+
+module.exports = app;
