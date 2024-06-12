7bcdeefba32ecb58543cdd547185a3e577b655a2
aezzahir <aero.glowsky@gmail.com>
2024-06-12 19:03:43 +0100

    6 - http express js

--- 0x05-Node_JS_basic/6-http_express.js
+++ 0x05-Node_JS_basic/6-http_express.js
@@ -3,9 +3,10 @@ const express = require('express');
 const app = express();
 const port = 1245;
 app.get('/', (req, res) => {
-  res.status(200).send(
-    'Hello Holberton School!',
-  );
+  res.status(200).send('Hello Holberton School!');
+});
+app.use((req, res, next) => {
+  res.status(404).send("Sorry can't find that!");
 });
 app.listen(port, () => {
   console.log(`App listening on port ${port}`);
