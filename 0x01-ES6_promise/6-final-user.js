f83ac2120c927ab9d93021880f09b560009ef497
aezzahir <aero.glowsky@gmail.com>
2025-07-19 19:21:17 +0100

    Throw error / try catch

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -4,20 +4,9 @@ import uploadPhoto from "./5-photo-reject";
 export default function handleProfileSignup(firstName, lastName, filename) {
   const promise1 = signUpUser(firstName, lastName);
   const promise2 = uploadPhoto(filename);
-  return Promise.allSettled([promise1, promise2]).then((results) => {
-    console.log("[");
-    results.forEach((result) => {
-      if (result.status === "fulfilled") {
-        console.log("{");
-        console.log(`status: '${result.status}',`);
-        console.log(
-          `value: { firstName: '${result.value.firstName}', lastName: '${result.value.lastName}' }`
-        );
-        console.log("}, ");
-      } else {
-        console.log(`{ status: '${result.status}', value:'${result.reason}' }`);
-      }
-    });
-    console.log("]");
-  });
-}
+  return Promise.allSettled([promise1, promise2]).then((results) => (
+    results.map(res => ({
+        status: res.status,
+        value: res.status === 'fulfilled' ? res.value : String(res.reason),
+    }))
+  ))};
