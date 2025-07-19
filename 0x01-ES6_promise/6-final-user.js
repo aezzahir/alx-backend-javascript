0dfe30571bcadc5ea23f96550506cb523678cb06
aezzahir <aero.glowsky@gmail.com>
2025-07-19 19:11:36 +0100

    Throw error / try catch

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -4,5 +4,20 @@ import uploadPhoto from "./5-photo-reject";
 export default function handleProfileSignup(firstName, lastName, filename) {
   const promise1 = signUpUser(firstName, lastName);
   const promise2 = uploadPhoto(filename);
-  return Promise.all([promise1, promise2])
+  return Promise.allSettled([promise1, promise2]).then((results) => {
+    console.log("[");
+    results.forEach((result) => {
+      if (result.status === "fulfilled") {
+        console.log("{");
+        console.log(`status: '${result.status}',`);
+        console.log(
+          `value: { firstName: '${result.value.firstName}', lastName: '${result.value.lastName}' }`
+        );
+        console.log("}, ");
+      } else {
+        console.log(`{ status: '${result.status}', value:'${result.reason}' }`);
+      }
+    });
+    console.log("]");
+  });
 }
